d9d9cfad5e3d02f4d41296f159271e7dff915122
jonpas <jonpas33@gmail.com>
2017-11-06 14:48:04 +0100

    Create Theseus theme (preliminary)

---
+++ resources/theseus/launcher_configuration.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+launcher_name = 'Theseus Launcher'
+default_mods_dir = 'tac_core'
+icon = 'icons/fl.ico'
+original_url = 'http://arma.theseus-aegis.com/repos/TheseusLauncher.exe'
+executable_name = 'Theseus_Launcher'
+forum_url = 'http://forum.frontline.frl/'
+discord_url = 'http://discord.frontline.frl'
+domain = 'arma.theseus-aegis.com'
+metadata_path = '/repos/metadata.json'
+torrents_path = '/repos/torrents'
+troubleshooting_url = 'https://github.com/overfl0/Bulletproof-Arma-Launcher/wiki/Troubleshooting'
+bugtracker_url = 'https://github.com/overfl0/Bulletproof-Arma-Launcher/issues'
+settings_directory = 'Theseus Launcher'
+news_url = 'http://arma.theseus-aegis.com/repos/news.md'
+dominant_color = '#eca92d'
+donate_url = 'https://patreon.com/frontline'
+capitalize_status = True
